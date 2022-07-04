9c12558ea093f897a3285470fac265238091f11d
Claudio Dotani <claudiodotani@gmail.com>
2022-07-04 11:58:50 +0200

    commit

--- lib/viewOrari.dart
+++ lib/viewOrari.dart
@@ -39,7 +39,6 @@ class OrariView extends StatefulWidget {
 // Create a corresponding State class.
 // This class holds data related to the form.
 class OrariViewState extends State<OrariView> {
-
   final url_base = "https://sad-spring.azurewebsites.net/";
 
   final _textControllerGiorno = TextEditingController();
@@ -47,12 +46,14 @@ class OrariViewState extends State<OrariView> {
   var _postsJson = [];
 
   void fetchPosts() async {
-
-
     try {
       print(widget.idCampo);
       String id = widget.idCampo;
-      final url = url_base + "disponibilitacampo/" + id + "/" + Giorno.millisecond.toString();
+      final url = url_base +
+          "disponibilitacampo/" +
+          id +
+          "/" +
+          Giorno.millisecond.toString();
       final response = await get(Uri.parse(url));
       print(response);
       final jsonData = jsonDecode(response.body) as List;
@@ -70,20 +71,22 @@ class OrariViewState extends State<OrariView> {
     fetchPosts();
   }
 
-  Future<void> _selectDate(BuildContext context) async {
-    print("Eccomi");
-    final DateTime? picked = await showDatePicker(
-        context: context,
-        initialDate: Giorno,
-        firstDate: DateTime.now(),
-        lastDate: DateTime(2101));
-    if (picked != null && picked != Giorno) {
-      setState(() {
-        Giorno = picked;
-        _textControllerGiorno.text = Giorno.toString();
-      });
+  void _selectDate(BuildContext context) async {
+    await showDatePicker(
+      context: context,
+      initialDate: Giorno,
+      firstDate: DateTime(2022),
+      lastDate: DateTime(2030),
+    ).then((selectedDate) {
+      if (selectedDate != null) {
+        setState(() {
+          Giorno = selectedDate;
+        });
+      }
     }
+    );
   }
+
 
 /*
   {
@@ -96,9 +99,16 @@ class OrariViewState extends State<OrariView> {
   }
 */
 
-
   void prenota(BuildContext context, int ora) async {
-    DateTime dataPartita = new DateTime(Giorno.year,Giorno.month, Giorno.day, ora + 2 , 0, 0, 0, 0);
+    DateTime dataPartita = new DateTime(
+        Giorno.year,
+        Giorno.month,
+        Giorno.day,
+        ora + 2,
+        0,
+        0,
+        0,
+        0);
 
     Map pren = {
       "durataPrenotazione": 60,
@@ -108,14 +118,16 @@ class OrariViewState extends State<OrariView> {
       "utente": 1,
       "privata": true
     };
-    String bPren= json.encode(pren);
+    String bPren = json.encode(pren);
     print(bPren);
     try {
-      String url = url_base + "insertPrenotazione" ;//Serve a passare l'id da una view all' altra
-      final response = await post(Uri.parse(url),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
+      String url = url_base +
+          "insertPrenotazione"; //Serve a passare l'id da una view all' altra
+      final response = await post(
+        Uri.parse(url),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
         body: bPren,
       );
       print(response.statusCode);
@@ -141,104 +153,73 @@ class OrariViewState extends State<OrariView> {
       ),
     );
   }
-DateTime date = DateTime(2022,07,04);
+
+  DateTime date = DateTime(2022, 07, 04);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: MaterialApp(
-          home: Scaffold(
-              backgroundColor: Colors.blue.shade400,
-              body: Stack(
-                children: <Widget>[
-                 Container(
-                   child:  Row(
-                     children: [
-                       GestureDetector(
-                         onTap: () {
-                           print("on prev");
-                         },
-                         child: Container(
-                           height: 20,
-                           width: 100,
-                           decoration: BoxDecoration(
-                               color: Colors.orange
-                           ),
-                           child: Text("Precedente"),
-                         ),
-                       ),
-                       Text("${Giorno.day} - ${Giorno.month} - ${Giorno.year}"),
-                       GestureDetector(
-                         onTap: () {
-                           print("on next");
-                         },
-                         child: Container(
-                           height: 20,
-                           width: 100,
-                           decoration: BoxDecoration(
-                               color: Colors.orange
-                           ),
-                           child: Text("Successivo"),
-                         ),
-                       ),
-                     ],
-                   ),
-                 ),
-                  ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemCount: _postsJson.length,
-                    itemBuilder: (context, i) {
-                      final post = _postsJson[i];
-                      return GestureDetector(
-                        onTap: () {
-                          prenota(context, post);
-                        },
-                        child: Container(
-                          height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                              color: Colors.orange,
-                              borderRadius: BorderRadius.circular(100)
-                          ),
-                          alignment: Alignment.center,
-                          child: Text(post.toString()),
-                        ),
-                      );
-                    },
-                  ),
-                ],
-              )
-          ),
-          //child: Text("${Giorno.day} - ${Giorno.month} - ${Giorno.year}")),
+      child: MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.blue.shade400,
+            body: Stack(
+              children: <Widget>[
 
-          FloatingActionButton(onPressed: (){
-
-          }, child: Icon(Icons.arrow_forward),),
-          /*ListView.builder(
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            itemCount: _postsJson.length,
-            itemBuilder: (context, i) {
-              final post = _postsJson[i];
-              return GestureDetector(
-                onTap: () {
-                  _showToast(context);
-                },
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(100)
-                  ),
-                  alignment: Alignment.center,
-                  child: Text(post.toString()),
+                ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: _postsJson.length,
+                  itemBuilder: (context, i) {
+                    final post = _postsJson[i];
+                    return GestureDetector(
+                      onTap: () {
+                        prenota(context, post);
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.circular(100)),
+                        alignment: Alignment.center,
+                        child: Text(post.toString()),
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
-          ),*/
-        ],
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                 children: [
+                   FloatingActionButton(
+                     onPressed: (){
+                       Giorno = new DateTime(Giorno.year, Giorno.month, Giorno.day-1);
+                       setState(() {});
+                       fetchPosts();
+                       },
+                     child: const Icon(Icons.arrow_back),
+                   ),
+
+
+                   ElevatedButton(
+                       onPressed: (){},
+                       child: Text("${Giorno.day} - ${Giorno.month} - ${Giorno.year}")
+                   ),
+
+                   FloatingActionButton(
+                     onPressed: (){
+                       Giorno = new DateTime(Giorno.year, Giorno.month, Giorno.day+1);
+                       setState(() {});
+                       fetchPosts();
+                     },
+                     child: const Icon(Icons.arrow_forward),
+                   ),
+                 ],
+                )
+              ],
+            )),
+        //child: Text("${Giorno.day} - ${Giorno.month} - ${Giorno.year}")),
       ),
-    ));
+    );
   }
 }
